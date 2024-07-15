ac9eb96254abc51cbb00a8c44e592cecbc2503b3
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-15 18:05:18 -0300

    refactor: router component

--- client/web-app/src/main.tsx
+++ client/web-app/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Router } from './router'
 import './app/styles/global.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>,
 )
--- client/web-app/src/router.tsx
+++ client/web-app/src/router.tsx
@@ -1,10 +1,12 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Home } from './app/pages/index/page'
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Home />} />),
-)
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
