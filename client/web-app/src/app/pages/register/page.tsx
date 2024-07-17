1ef327e4020615b1c8ea297ddc130de8357876fc
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-17 00:44:59 -0300

    feat[RegisterPage]: add form

--- client/web-app/src/app/pages/register/page.tsx
+++ client/web-app/src/app/pages/register/page.tsx
@@ -1,34 +1,78 @@
+import { useForm } from 'react-hook-form'
 import { Text } from '../../components/common/Text'
 import { Button } from '../../components/forms/Button'
 import { Input } from '../../components/forms/Input'
+import { RegisterUserSchema, RegisterUserSchemaType } from './schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect } from 'react'
 
 export function RegisterPage() {
+  const {
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    register,
+  } = useForm<RegisterUserSchemaType>({
+    resolver: zodResolver(RegisterUserSchema),
+  })
+
+  useEffect(() => {
+    console.log({ errors, data: getValues() })
+  }, [errors])
+
+  async function onValidSubmit(data: RegisterUserSchemaType) {
+    console.log({ data })
+  }
+
   return (
     <div className="body-df">
-      <main className="mx-auto max-w-96 pt-32 pb-8">
+      <main className="mx-auto max-w-96 pt-16 pb-12">
         <h1 className="text-center flex gap-x-2 justify-center">
           <img src="./logo-white.svg" alt="GPT logo" />
-          <Text variant="title">Create Account</Text>
+          <Text variant="title">GPTCUTS</Text>
         </h1>
-        <form className="mt-8 flex flex-col gap-y-4">
+        <form
+          className="mt-8 flex flex-col gap-y-3"
+          onSubmit={handleSubmit(onValidSubmit)}
+        >
           <Input.Root>
             <Input.Label>Username:</Input.Label>
-            <Input.Box type="text" placeholder="Type your username" />
+            <Input.Box
+              type="text"
+              placeholder="Type your username"
+              errorMessage={errors.username?.message}
+              {...register('username')}
+            />
           </Input.Root>
           <Input.Root>
             <Input.Label>Email:</Input.Label>
-            <Input.Box type="text" placeholder="Type your email" />
+            <Input.Box
+              type="text"
+              placeholder="Type your email"
+              errorMessage={errors.email?.message}
+              {...register('email')}
+            />
           </Input.Root>
           <Input.Root>
             <Input.Label>Password:</Input.Label>
-            <Input.Box type="password" placeholder="Type your password" />
+            <Input.Box
+              type="password"
+              placeholder="Type your password"
+              errorMessage={errors.password?.message}
+              {...register('password')}
+            />
           </Input.Root>
           <Input.Root>
             <Input.Label>Confirm Password:</Input.Label>
-            <Input.Box type="password" placeholder="Type your password again" />
+            <Input.Box
+              type="password"
+              placeholder="Type your password again"
+              errorMessage={errors.confirm_password?.message}
+              {...register('confirm_password')}
+            />
           </Input.Root>
-          <div className="mt-4 w-full">
-            <Button>Register</Button>
+          <div className="mt-6 w-full">
+            <Button type="submit">Create Account</Button>
           </div>
         </form>
       </main>
