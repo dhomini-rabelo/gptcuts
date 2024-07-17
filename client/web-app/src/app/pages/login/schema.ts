786a4dc2df4165f59cc3808a702587f99a2667ba
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-17 01:50:34 -0300

    fix[LoginSchema]: update min text to 1

--- client/web-app/src/app/pages/login/schema.ts
+++ client/web-app/src/app/pages/login/schema.ts
@@ -3,7 +3,7 @@ import * as zod from 'zod'
 
 export const LoginSchema = zod.object({
   email: zod.string().min(1, ErrorMessages.REQUIRED),
-  password: zod.string().min(8, DynamicErrorMessages.minLength(8)),
+  password: zod.string().min(1, DynamicErrorMessages.minLength(1)),
 })
 
 export type LoginSchemaType = zod.infer<typeof LoginSchema>
