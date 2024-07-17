65fd6544988282a82cc1e5ae63d243b1502e5671
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-17 11:49:16 -0300

    fix[InputBox]: rm InputMessage

--- client/web-app/src/app/components/forms/Input/Box/index.tsx
+++ client/web-app/src/app/components/forms/Input/Box/index.tsx
@@ -1,35 +1,16 @@
-import { XCircle } from 'phosphor-react'
-import { Text } from '../../../common/Text'
 import { Div } from './style'
-import { colors } from '../../../../tokens/colors'
 import { forwardRef } from 'react'
 
 type InputBoxProps = React.InputHTMLAttributes<HTMLInputElement> & {
   type?: 'text' | 'password'
-  errorMessage?: string
 }
 
 export const InputBox = forwardRef<HTMLInputElement, InputBoxProps>(
-  ({ errorMessage, type = 'text', spellCheck = false, ...inputProps }, ref) => {
+  ({ type = 'text', spellCheck = false, ...inputProps }, ref) => {
     return (
-      <>
-        <Div.InputBox>
-          <input
-            ref={ref}
-            type={type}
-            spellCheck={spellCheck}
-            {...inputProps}
-          />
-        </Div.InputBox>
-        {errorMessage && (
-          <div className="flex items-center gap-x-1">
-            <XCircle size={16} color={colors['Red-400']} />
-            <Text color="Red-400" size="sm" weight="regular">
-              {errorMessage}
-            </Text>
-          </div>
-        )}
-      </>
+      <Div.InputBox>
+        <input ref={ref} type={type} spellCheck={spellCheck} {...inputProps} />
+      </Div.InputBox>
     )
   },
 )
