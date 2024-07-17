2379d93254067ec46f38b7200aed2edaa3cd2e97
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-17 15:47:48 -0300

    feat[/my-shortcuts]: add folder

--- client/web-app/src/app/pages/my-shortcuts/page.tsx
+++ client/web-app/src/app/pages/my-shortcuts/page.tsx
@@ -1,4 +1,6 @@
+import { Plus } from 'phosphor-react'
 import { Text } from '../../components/common/Text'
+import { Button } from '../../components/forms/Button'
 import { Folder } from '../../components/general/Folder'
 import { Header } from '../../components/utils/Header'
 
@@ -7,14 +9,28 @@ export function MyShortcutsPage() {
     <div className="body-df">
       <Header />
       <main className="w-full max-w-[70rem] mx-auto px-8">
-        <div className="text-center">
-          <h1 className="mt-12">
-            <Text variant="title">MY SHORTCUTS</Text>
-          </h1>
+        <div className="mt-12 flex items-center justify-between">
+          <div className="flex flex-col">
+            <h1>
+              <Text variant="title">MY SHORTCUTS</Text>
+            </h1>
+            <div>
+              <Text size="sm" weight="regular" color="Gray-700">
+                @someuser
+              </Text>
+            </div>
+          </div>
           <div>
-            <Text size="sm" weight="regular" color="Gray-700">
-              @someuser
-            </Text>
+            <Button type="submit">
+              <span className="flex items-center">
+                <Plus
+                  size={16}
+                  weight="bold"
+                  className="inline-block mr-1 relative -top-[0.5px]"
+                />
+                Add folder
+              </span>
+            </Button>
           </div>
         </div>
         <section className="folders grid grid-cols-2 mt-8 gap-x-4 gap-y-8 pb-12">
