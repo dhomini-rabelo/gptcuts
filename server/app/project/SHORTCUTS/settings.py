220be33c51d59db389ea296d902268eca31d0a25
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-18 00:51:45 -0300

    feat: add shortcuts app

--- server/app/project/SHORTCUTS/settings.py
+++ server/app/project/SHORTCUTS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.accounts.AccountsConfig',
+    'apps.shortcuts.ShortcutsConfig',
 ]
 
 MIDDLEWARE = [
