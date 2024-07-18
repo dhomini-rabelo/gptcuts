100724f180310283a6a86a330d566bb8b0e582b1
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-17 23:28:29 -0300

    feat: add accounts app

--- server/app/project/SHORTCUTS/settings.py
+++ server/app/project/SHORTCUTS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Extra settings
+
+AUTH_USER_MODEL = 'accounts.User'
