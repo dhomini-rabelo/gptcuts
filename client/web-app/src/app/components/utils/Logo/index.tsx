e4d9ab5fdce9d8f9f2ee7e2663b65f43f5a109e6
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-18 12:39:06 -0300

    fix[Logo]: add link to home

--- client/web-app/src/app/components/utils/Logo/index.tsx
+++ client/web-app/src/app/components/utils/Logo/index.tsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom'
 import { Text } from '../../../components/common/Text'
 
 export function Logo() {
   return (
-    <div className="text-center flex gap-x-2 justify-center items-center">
-      <img src="./logo-white.svg" alt="GPT logo" />
-      <strong>
-        <Text variant="title">GPTCUTS</Text>
-      </strong>
-    </div>
+    <Link to="/">
+      <div className="text-center flex gap-x-2 justify-center items-center">
+        <img src="./logo-white.svg" alt="GPT logo" />
+        <strong>
+          <Text variant="title">GPTCUTS</Text>
+        </strong>
+      </div>
+    </Link>
   )
 }
