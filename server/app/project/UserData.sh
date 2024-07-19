a347f5281cccf914cd56ff0afa107e27304f1c4f
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-07-19 09:15:02 -0300

    minor fix

--- server/app/project/UserData.sh
+++ server/app/project/UserData.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Replace {YOUR_GIT_REOPO_URL} with your actual Git repository URL
-GIT_REPO_URL="https://github.com/dhomini-rabelo/gptcuts.git"
+# GIT_REPO_URL="https://github.com/dhomini-rabelo/gptcuts.git"
 
 # If using Private Repo
 #GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
@@ -11,12 +11,12 @@ GIT_REPO_URL="https://github.com/dhomini-rabelo/gptcuts.git"
 PROJECT_MAIN_DIR_NAME="gptcuts"
 
 # Clone repository
-git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
+# git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
 
-cd "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/server/app/project"
+# cd "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/server/app/project"
 
 # Make all .sh files executable
-chmod +x scripts/*.sh
+# chmod +x scripts/*.sh
 
 # Execute scripts for OS dependencies, Python dependencies, Gunicorn, Nginx, and starting the application
 touch filenamel_00.txt
